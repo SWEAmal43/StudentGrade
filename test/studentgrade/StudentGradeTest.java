5b82ec3aba33f1e9af132361b87596d460c40357
SWEAmal43 <Amalalsharari77@gmail.com>
2025-03-20 07:42:51 +0300

    Final update for assignment

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,22 +1,18 @@
 package studentgrade;
 
-import junit.framework.TestCase;
-
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class StudentGradeTest {
     
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
-
+    @Test
     public void testGetGrade95() {
         System.out.println("getGrade 95");
         int mark = 95;
         String expResult = "A";
         String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+       assertEquals(expResult, result);
     }
-
+    @Test
     public void testGetGrade85() {
         System.out.println("getGrade 85");
         int mark = 85;
@@ -24,12 +20,15 @@ public class StudentGradeTest extends TestCase {
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
     }
-    
     public void testGetGrade75() {
         System.out.println("getGrade 85");
         int mark = 75;
         String expResult = "F";
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
+    }
+
+    private void assertEquals(String expResult, String result) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
